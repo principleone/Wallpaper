b40200b2040b1d0b4a2ec278349810b2357210ee
adj97 <50122409+adj97@users.noreply.github.com>
2020-10-22 12:49:45 +0100

    Add files via upload

---
+++ IntuneWallpaperPush.ps1
@@ -0,0 +1,67 @@
+﻿$RegKeyPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+$sysLightBool = (Get-ItemProperty -Path $RegKeyPath -erroraction silentlycontinue).SystemUsesLightTheme 
+
+# chose light or dark image path
+if ($sysLightBool -eq 0)
+{
+    #System is in dark mode
+    $url = "https://raw.githubusercontent.com/principleone/Wallpaper/main/P1Dark.jpg"
+} else {
+    #System is in light mode
+    $url = "https://raw.githubusercontent.com/principleone/Wallpaper/main/P1Light.jpg"
+}
+
+$WallpaperURL = $url
+$LockscreenUrl = $url
+
+$ImageDestinationFolder = "C:\P1\Wallpaper" # Change to your fitting - this is the folder for the wallpaper image
+md $ImageDestinationFolder -erroraction silentlycontinue # Creates the destination folder on the target computer
+
+$WallpaperDestinationFile = "$ImageDestinationFolder\wallpaper.png" # Change to your fitting - this is the Wallpaper image
+$LockScreenDestinationFile = "$ImageDestinationFolder\LockScreen.png" # Change to your fitting - this is the Lockscreen image
+
+# Downloads the image file from the source location
+Start-BitsTransfer -Source $WallpaperURL -Destination "$WallpaperDestinationFile"
+Start-BitsTransfer -Source $LockscreenUrl -Destination "$LockScreenDestinationFile"
+
+# Assigns the wallpaper 
+$RegKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+
+$DesktopPath = "DesktopImagePath"
+$DesktopStatus = "DesktopImageStatus"
+$DesktopUrl = "DesktopImageUrl"
+$LockScreenPath = "LockScreenImagePath"
+$LockScreenStatus = "LockScreenImageStatus"
+$LockScreenUrl = "LockScreenImageUrl"
+
+$StatusValue = "1"
+$DesktopImageValue = "$WallpaperDestinationFile"  
+$LockScreenImageValue = "$LockScreenDestinationFile"
+
+IF(!(Test-Path $RegKeyPath))
+
+{
+
+New-Item -Path $RegKeyPath -Force | Out-Null
+
+New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+
+}
+
+ELSE {
+
+New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $value -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+}
+
+# Clears the error log from powershell before exiting
+    $error.clear()
