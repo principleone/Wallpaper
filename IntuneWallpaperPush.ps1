507f5348327e5c2fca5f51e7319787939435406a
adj97 <50122409+adj97@users.noreply.github.com>
2020-10-26 09:15:18 +0000

    Update IntuneWallpaperPush.ps1

--- IntuneWallpaperPush.ps1
+++ IntuneWallpaperPush.ps1
@@ -1,5 +1,5 @@
-﻿$RegKeyPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$sysLightBool = (Get-ItemProperty -Path $RegKeyPath -Name "SystemUsesLightTheme" -ErrorAction SilentlyContinue).SystemUsesLightTheme 
+$RegKeyPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+$sysLightBool = (Get-ItemProperty -Path $RegKeyPath -Name "SystemUsesLightTheme").SystemUsesLightTheme 
 
 # chose light or dark image path
 if ($sysLightBool -eq 0)
@@ -12,58 +12,27 @@ if ($sysLightBool -eq 0)
 }
 
 $WallpaperURL = $url
-# $LockscreenUrl = $url
 
 $ImageDestinationFolder = "C:\P1\Wallpaper" # Change to your fitting - this is the folder for the wallpaper image
 md $ImageDestinationFolder -erroraction silentlycontinue # Creates the destination folder on the target computer
+attrib +h "C:\P1"
 
 $WallpaperDestinationFile = "$ImageDestinationFolder\wallpaper.png" # Change to your fitting - this is the Wallpaper image
-# $LockScreenDestinationFile = "$ImageDestinationFolder\LockScreen.png" # Change to your fitting - this is the Lockscreen image
 
 # Downloads the image file from the source location
 Start-BitsTransfer -Source $WallpaperURL -Destination "$WallpaperDestinationFile" -erroraction silentlycontinue
-# Start-BitsTransfer -Source $LockscreenUrl -Destination "$LockScreenDestinationFile" -erroraction silentlycontinue
 
 # Assigns the wallpaper 
 $RegKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
 
-$DesktopPath = "DesktopImagePath"
-$DesktopStatus = "DesktopImageStatus"
-$DesktopUrl = "DesktopImageUrl"
-$LockScreenPath = "LockScreenImagePath"
-$LockScreenStatus = "LockScreenImageStatus"
-$LockScreenUrl = "LockScreenImageUrl"
-
-$DesktopStatusValue = "1"
-$LockScreenStatusValue = "0"
-$DesktopImageValue = "$WallpaperDestinationFile"  
-# $LockScreenImageValue = "$LockScreenDestinationFile"
-
-IF(!(Test-Path $RegKeyPath))
-
-{
-
+Remove-Item -Path $RegKeyPath -Recurse -erroraction silentlycontinue
 New-Item -Path $RegKeyPath -Force | Out-Null
 
-New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $DesktopStatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $LockScreenStatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-Remove-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -erroraction silentlycontinue
-Remove-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -erroraction silentlycontinue
+New-ItemProperty -Path $RegKeyPath -Name "DesktopImageStatus" -Value "1" -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name "LockScreenImageStatus" -Value "0" -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name "DesktopImagePath" -Value $WallpaperDestinationFile -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name "DesktopImageUrl" -Value $WallpaperDestinationFile -PropertyType STRING -Force | Out-Null
 
-}
-
-ELSE {
-
-New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $DesktopStatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $LockScreenStatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
-Remove-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -erroraction silentlycontinue
-Remove-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -erroraction silentlycontinue
-
-}
 
 # Clears the error log from powershell before exiting
     $error.clear()
